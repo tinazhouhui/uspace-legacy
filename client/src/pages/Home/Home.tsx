8a09b6e2d088d1b4adcf3f7e039c771728163707
Tina <tina.zhou.hui@gmail.com>
2022-05-03 12:56:50 +0100

    feat: user context

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,50 +1,77 @@
 import Header from '../../components/Header/Header';
 import Loading from '../../components/Loading/Loading';
-import { useAuth0 } from '@auth0/auth0-react';
+import {useAuth0} from '@auth0/auth0-react';
 import Spaces from './components/Spaces/Spaces';
-import { CreateUserType } from '../../interfaces/Interfaces';
-import { useState } from 'react';
+import {CreateUserType, UserType} from '../../interfaces/Interfaces';
+import {createContext, useEffect, useState} from 'react';
 import Welcome from './components/Welcome/Welcome';
 import API_USER_SERVICE from '../../services/apiUserService';
 
+
+function userExists(id: string) {
+    console.log('CHECK IF EXISTS')
+    return API_USER_SERVICE.getUserById(id)
+        .then((response) => {
+            return !!response;
+        })
+}
+
+const UserContext = createContext<UserType | {}>({})
+
 function Home() {
-  const { isLoading, isAuthenticated, user } = useAuth0();
-  const [opened, setOpened] = useState<boolean>(false);
+    const {isLoading, isAuthenticated, user} = useAuth0();
+    const [opened, setOpened] = useState<boolean>(false);
+    const [userData, setUser] = useState<UserType | {}>({})
 
-  if (!isLoading && user) {
+    useEffect(() => {
+        console.log('_________________')
+        console.log('loading', isLoading);
+        console.log('user', user);
+        console.log('authenticated', isAuthenticated);
+        if (user) {
+            if (!userExists(user.sub!)) {
+                const userData: CreateUserType = {
+                    email: user!.email!,
+                    email_verified: user!.email_verified!,
+                    username: user!.nickname!,
+                    picture_url: user!.picture!,
+                    sub: user!.sub!,
+                };
 
-    const userData: CreateUserType = {
-      email: user!.email!,
-      email_verified: user!.email_verified!,
-      username: user!.nickname!,
-      picture_url: user!.picture!,
-      sub: user!.sub!,
-    };
-    //todo creates user EVERYTIME
-    API_USER_SERVICE.createUser(userData)
-        .then((data) => {console.log(data)})
-        .catch((err) => {console.error(err)})
-  }
+                API_USER_SERVICE.createUser(userData)
+                    .then((data) => {
+                        console.log(data)
+                    })
+                    .catch((err) => {
+                        console.error(err)
+                    })
+            }
+            setUser(user);
+        }
+    }, [user])
 
-  //todo can be done using ? with one return
-  if (isLoading) {
+
+    //todo can be done using ? with one return
+    if (isLoading) {
+        return (
+            <div className="main-loading">
+                <Loading/>
+            </div>
+        );
+    }
+
     return (
-      <div className="main-loading">
-        <Loading />
-      </div>
+        <>
+            <Header setOpened={setOpened}/>
+            <UserContext.Provider value={userData}>
+                {isAuthenticated ? (
+                    <Spaces opened={opened} setOpened={setOpened}/>
+                ) : (
+                    <Welcome/>
+                )}
+            </UserContext.Provider>
+        </>
     );
-  }
-
-  return (
-    <>
-      <Header setOpened={setOpened} />
-      {isAuthenticated ? (
-        <Spaces opened={opened} setOpened={setOpened} />
-      ) : (
-        <Welcome />
-      )}
-    </>
-  );
 }
 
 export default Home;
--- client/src/services/apiUserService.tsx
+++ client/src/services/apiUserService.tsx
@@ -3,18 +3,33 @@ import {CreateUserType,} from '../interfaces/Interfaces';
 const URL = process.env.REACT_APP_API;
 
 const getUserById = async (id: string) => {
-    const res = await fetch(URL + `/users/${id}`);
-    return await res.json();
+    try {
+        const res = await fetch(URL + `/users/${id}`);
+        if (res.status < 300) {
+            return await res.json();
+        } else {
+            const error = await res.json()
+            console.error({ status: res.status, error});
+        }
+    } catch (e) {
+        console.error('Fetch error', e);
+    }
 }
 // creates a single user
 const createUser = async (data: CreateUserType) => {
-    await fetch(URL + '/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
+    try {
+        await fetch(URL + '/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        return {message: 'User successfully created'}
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 const API_USER_SERVICE = {getUserById, createUser}
